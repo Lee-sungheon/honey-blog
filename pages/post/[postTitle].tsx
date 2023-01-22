c7a4b48bd5c3bfc67a5108bd8ac8e3a2331c1f11
Lee-sungheon <leesungheon.dev@gmail.com>
2023-01-22 18:36:55 +0900

    chore: dynamic 제거, ssr 에러 페이지 보내기, tsconfig path 설정

--- pages/post/[postTitle].tsx
+++ pages/post/[postTitle].tsx
@@ -1,22 +1,26 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import dynamic from 'next/dynamic';
-
-const MarkdownViewer = dynamic(() => import('../../components/MarkdownViewer'), {
-  ssr: false,
-});
+import MarkdownViewer from '@components/MarkdownViewer';
 
 export default function PostPage({ markdown }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return <MarkdownViewer markdown={markdown} />;
 }
 
 export const getServerSideProps: GetServerSideProps<{ markdown: string }> = async (ctx) => {
-  const postTitle = (ctx.query['postTitle'] as string) ?? '';
-  const fs = await import('fs');
-  const markdown = fs.readFileSync(`markdown/${postTitle}.md`, 'utf8');
+  try {
+    const postTitle = (ctx.query['postTitle'] as string) ?? '';
+    const fs = await import('fs');
+    const markdown = fs.readFileSync(`markdown/${postTitle}.md`, 'utf8');
 
-  return {
-    props: {
-      markdown: markdown,
-    },
-  };
+    return {
+      props: { markdown: markdown },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      redirect: {
+        destination: '/post/404',
+        permanent: false,
+      },
+    };
+  }
 };
